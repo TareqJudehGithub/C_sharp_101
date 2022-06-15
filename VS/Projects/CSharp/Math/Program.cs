e317b3d9e7bb6645a5a8022d470bbbf032236f61
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-06-15 19:03:09 +0200

    Completed Operators, Math, and Input lectures and exercises in C# W3Schools tutorial.

---
+++ VS/Projects/CSharp/Math/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Math in C#");
+
+Console.WriteLine("Math.Max()");
+
+int x = 9, y = 10;
+
+int maxValue = Math.Max(x, y);
+Console.WriteLine($"the highest value between {x} and {y} is {maxValue}\n");
+
+Console.WriteLine("Math.Min()");
+
+int minValue = Math.Min(x, y);
+Console.WriteLine($"the lowest value between {x} and {y} is {minValue}\n");
+
+Console.WriteLine("Math.Sqrt()");
+
+Console.WriteLine($"The square root of {x} is {Math.Sqrt(x)}");
+
+
---
+++ VS/Projects/CSharp/userInput/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Hi, what is your name? ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string name = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+// other data types inputs
+Console.WriteLine("What is your age? ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Hi, {name}! You're {age} old today.");
+
