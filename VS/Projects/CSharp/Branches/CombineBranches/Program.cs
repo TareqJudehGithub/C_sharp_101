ba33a3184b5a226780255e325b8307fd9ab54eb3
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-06-18 18:51:43 +0200

    Completed all for and while loops exercises.

---
+++ VS/Projects/CSharp/Branches/CombineBranches/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Combine branches and loops");
+
+// find the sum of all integers 1 through 20 that are divisible by 3.
+int sum = 0;
+
+for (int i = 1; i <= 20; i++)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} is divisible by 3. {i} / 3 = {i / 3}");
+        sum += i;
+        Console.WriteLine($"sum = {sum}\n");
+    }
+}
+Console.WriteLine($"sum total = {sum}");
+
+
---
+++ VS/Projects/CSharp/Branches/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Branches
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If statements");
+            int a = 15;
+            int b = 25;
+
+            if (a > b)
+            {
+                Console.WriteLine($"{a} is greater than {b}");
+            }
+            else
+            {
+                Console.WriteLine($"{b} is greater than {a}");
+            }
+            Console.WriteLine(" ");
+
+            Console.Write("Weather today: ");
+            string weather = "warm";
+            bool isRaining = false;
+
+            // Check what to wear 
+            if ((weather == "warm") && (isRaining == false))
+            {
+                Console.WriteLine("It's OK to wear a t-shirt today! Enjoy the sum!");
+            }
+            else if ((weather == "cold") && (isRaining == false))
+            {
+                Console.WriteLine("Wear a jacket it's going to be cold today.");
+            }
+            else if ((weather == "cold") && (isRaining == true)) 
+            {
+                Console.WriteLine("Take an umbrella it's raining today!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry or user values.");
+            }
+
+            // Loops
+            Console.WriteLine("while loop");
+            int num = 0;
+            while (num < 10)
+            {
+                Console.WriteLine($"{num} is less  than 10");
+                num++;
+            }
+            Console.WriteLine("End of while loop");
+
+            // for loop
+            Console.WriteLine("\nfor loop\n");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i} is less than 10");
+            }
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }
+            
+
+
+        }
+
+    }
+}
