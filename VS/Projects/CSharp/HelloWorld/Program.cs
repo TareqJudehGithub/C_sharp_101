e580b23e5ba72f32d23db3296500b299af7488b2
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-06-20 00:39:03 +0200

    Completed Methods section: Calling methods, Method overloading, and Method returns. Completed Method section challenge.

--- VS/Projects/CSharp/HelloWorld/Program.cs
+++ VS/Projects/CSharp/HelloWorld/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(5 / 10);
+
+// Instantiating a new object from Random class storing it in a new variable
+Random dice = new Random();
+
+// Calling Next() method passing it two parameters (min, and max) values, saving 
+// the result in a new variable
+int diceOne = dice.Next(1, 6);
+
+Console.WriteLine(diceOne);
 
