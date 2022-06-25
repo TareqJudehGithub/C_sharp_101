fa4ab4adeef80a7cc3fd7c3212398fad2e1084fd
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-06-26 00:21:02 +0200

    Completed Operators and Conditional lectures - C# Console and Forms course.

---
+++ VS/Projects/CSharp/ConsoleProgramming/ConditionalStatements/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Conditional Statements");
+
+Console.Write("Enter first number: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+// if statements (== < > <== >== !=)
+if (firstNum > secondNum)
+{
+    Console.WriteLine($"{firstNum} is greater than {secondNum}");
+}
+else if (firstNum == secondNum)
+{
+    Console.WriteLine($"{firstNum} is the same as {secondNum}");
+}
+else
+{
+    Console.WriteLine($"{firstNum} is lesser than {secondNum}");
+}
+// switch statement
+Console.Write("What's today's weather? ");
+string weather = Console.ReadLine();
+
+switch (weather)
+{
+    case "sunny":
+        Console.WriteLine("Wear a T-shirt today!");
+        break;
+    case "cold":
+        Console.WriteLine("Wear a jacket it is going to be cold today!");
+        break;
+    case "rain":
+        Console.WriteLine("Take an umbrella!");
+        break;
+    default:
+        Console.WriteLine("Check the weather forecast before you go out today.");
+        break;
+}
+
+// Ternary Operator
+Console.Write("Guess my secret number: ");
+int secretNum = Convert.ToInt32(Console.ReadLine());
+var yourGuess = secretNum == 25 ? $"You guessed {secretNum}! Correct!" : $"{secretNum} is a wrong answer.";
+
+Console.WriteLine(yourGuess);
+
+Console.WriteLine(yourGuess.Equals("Hello!"));
---
+++ VS/Projects/CSharp/ConsoleProgramming/MathOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Basic Operators
+int num; // Declare a variable
+num = 5; // Assign a value to a variable
+
+int num1 = 11;
+int num2 = 12;
+Console.WriteLine($"num1 = {num1}");
+
+
+// Arithmetic Operators
+Console.WriteLine($"Addition: {num1} + {num2} = {num1 + num2}");
+
+// Compound Operators
+num += 5;
+
+
+
+Console.WriteLine($"num1 = {num1}");
