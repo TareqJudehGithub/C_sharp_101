0ebd8d4b7a6c3a8a1348402427bb20cd77162990
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-07-05 21:50:55 +0200

    Completed Strings and DateTime lectures.

---
+++ VS/Projects/CSharp/ConsoleProgramming/DateTimeManipulation/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+Console.WriteLine("Date Time Manipulation\n");
+
+// Empty DateTime 
+// Initialize a new object from DateTime class
+DateTime date = new DateTime();
+
+
+// Instantiate a new variable from dateTime class
+DateTime dateOfBirth = new DateTime(1975, 6, 27);
+Console.WriteLine($"I was born in {dateOfBirth}");
+
+DateOnly dob = DateOnly.FromDateTime(dateOfBirth); // Date only
+TimeOnly tob = TimeOnly.FromDateTime(dateOfBirth); // Time only
+Console.WriteLine($"I was born in {dob}");
+Console.WriteLine($"I was born in {tob}");
+
+
+// Create a DateTime from current time stamp
+DateTime now = DateTime.Now;
+Console.WriteLine($"The time now is {now}");
+
+
+// Create a DateTime from a string
+DateTime dateFromString = DateTime.Parse("1/31/2021", CultureInfo.InvariantCulture);
+Console.WriteLine($"The date from string is {dateFromString}");
+
+// Add additional time
+Console.WriteLine($"One hour from now is: {now.AddHours(1)}");
+Console.WriteLine($"One day from now is: {now.AddDays(1)}");
+
+// Ticks from DateTime
+Console.WriteLine("Time as numeral: " + now.Ticks);
+
+
+
+Console.WriteLine("\nDate Time\n");
+
+DateTime localDate = DateTime.Now;
+
+Console.WriteLine($"The date now is {DateOnly.FromDateTime(localDate)}"); // Today's date
+Console.WriteLine($"The date now is {TimeOnly.FromDateTime(localDate)}"); // Time's now
+Console.WriteLine($"UTC time now is {localDate}"); // Both date and time
+
+
---
+++ VS/Projects/CSharp/ConsoleProgramming/Strings/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("String Manipulation");
+
+string firstName = "John";
+string lastName = "Smith";
+DateTime date = DateTime.Now;
+
+
+// Print to string
+Console.WriteLine($"My first name is {firstName}\n");
+
+
+// Concatenation & interpolation
+Console.WriteLine("My name is {0} {1}.\n", firstName, lastName);
+Console.WriteLine($"My name is {firstName} {lastName}\n");
+
+// String Length
+int nameLength = firstName.Length;
+Console.WriteLine($"Your first name is {nameLength} characters long.\n");
+
+
+// Replace String parts
+string newFirstName = firstName.Replace("John", "John K.");
+Console.WriteLine($"Your new first name is {newFirstName}\n");
+
+
+// Append to other string variable
+string fullName = newFirstName + " " + lastName;
+Console.WriteLine($"Your fill name is {fullName}\n");
+
+
+// Split string
+string[] splitFullName = fullName.Split(' ');
+for (int i = 0; i < splitFullName.Length; i++)
+{
+    Console.WriteLine(splitFullName[i]);
+}
+
+
+
+// Compare String
+Console.WriteLine("String manipulation: \n");
+string nullVariable = null;
+string emptyString = "";
+string whiteSpace = " ";
+
+
+Console.WriteLine(string.IsNullOrEmpty(nullVariable));      // true
+Console.WriteLine(string.IsNullOrWhiteSpace(emptyString));  // true
+Console.WriteLine(string.IsNullOrWhiteSpace(whiteSpace));   // true
+Console.WriteLine(string.IsNullOrWhiteSpace(firstName));    // true
+
+int stringCompare = string.Compare("hello", "hello");       // 0 => equal
+Console.WriteLine(stringCompare);
+int stringCompare2 = string.Compare("hello", "Hello");       // -1 => not equal
+
+// convert to string
+int number = 123;
+Console.WriteLine(number + Convert.ToString(number)); // 123123
+
+
