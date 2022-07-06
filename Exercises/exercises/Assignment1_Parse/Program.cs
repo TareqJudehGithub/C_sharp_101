f118e5c17c423dcc040508c8b692add82d71bcf7
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-07-06 22:08:01 +0200

    Adding exercises folder

---
+++ Exercises/exercises/Assignment1_Parse/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Assignment 1\n");
+string stringForFloat = "0.85"; // datatype should be float
+string stringForInt = "12345"; // datatype should be int
+
+double strToInt = Convert.ToDouble(stringForFloat);
+
+Console.WriteLine();
+
+// float intToFloat = (float)strToInt;
+// Console.WriteLine(intToFloat);
+
+
+// int intValue = Convert.ToInt32(stringForInt);
