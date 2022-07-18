aa74516aeafdd7bb83b509cc655c35daf2511242
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-07-19 01:32:37 +0200

    Completed OOP introduction to classes and objects.

---
+++ OOP/Human.cs
@@ -0,0 +1,16 @@
+﻿namespace OOP
+{
+    // This class is blueprint for a data type
+    internal class Human
+    {
+        // member variable
+        public string firstName = string.Empty;     // Empty or we could set a default value.
+        public string lastName = string.Empty;
+
+        // member method
+        public void IntroduceMySelf()
+        {
+            Console.WriteLine($"Hi, I'm {firstName} {lastName}!");
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OOP\n");
+
+            // Instantiate a new object from class Human;
+            Human denis = new Human();
+            Human micheal = new Human();
+
+            // access variables from outside this class, using 'public' access modifier
+            denis.firstName = "Denis";
+            denis.lastName = "Rodman";
+            micheal.firstName = "Micheal";
+            micheal.lastName = "Adams";
+
+            // calling a method from Human class.
+            denis.IntroduceMySelf();
+            micheal.IntroduceMySelf();
+
+
+        }
+    }
+}
