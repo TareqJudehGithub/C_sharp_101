46cc3662967eef846354e6df15b4016c7de4594b
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-07-20 17:45:20 +0200

    Added Switch statement folder.

---
+++ ControlVarScopeUsingCodeBlocks/MethodsClassesNamespaces/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MethodsClassesNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Methods in c#\n");
+            string word = "Microsoft Learn";
+
+            Console.WriteLine(Reverse(word));
+        }
+        static string Reverse(string message)
+        {
+            char[] letter = message.ToCharArray();
+            Array.Reverse(letter);
+            return new string(letter);
+        }
+    }
+}
+
---
+++ ControlVarScopeUsingCodeBlocks/NoCodeBlocks/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("No code blocks needed.");
+
+string name = "bobs";
+if (name.Equals("steve")) Console.WriteLine($"{Char.ToUpper(name[0]) + name.Substring(1)} is here!");
+else if (name.Equals("bob")) Console.WriteLine("Bob is here!");
+else Console.WriteLine("Hello, stranger!");
