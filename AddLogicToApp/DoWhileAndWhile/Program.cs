9f5d458bf1023e9409b068e10428d032a083d3db
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-07-22 01:59:58 +0200

    Completed do-while and while loop and section main challenge.

---
+++ AddLogicToApp/DoWhileAndWhile/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("do-while loop\n");
+
+int number = 0;
+
+do
+{
+    Console.WriteLine($"number before incrementing: {number}");
+    number++;
+    Console.WriteLine($"number inside do block of code is: {number}");
+}
+while (number < 3);
+
+
+Console.WriteLine("write code that will keep generating random numbers between 1 and 10 until we generate the number 7. \n");
+
+Random dice = new Random();
+int diceRoll = 0;
+
+do
+{
+
+    Console.WriteLine($"Roll the dice! {diceRoll}");
+    diceRoll = dice.Next(1, 11);
+}
+while (diceRoll != 7);
+
+
+Console.WriteLine("Write a while statement that iterates only while a random number is greater than some value\n");
+
+while (diceRoll >= 3)
+{
+    diceRoll = dice.Next(1, 11);
+    Console.WriteLine($"{diceRoll} is still greater than 3");
+}
+Console.WriteLine(diceRoll + " End of while loop\n");
+
+Console.WriteLine("continue keyword");
+int randNum = dice.Next(1, 7);
+
+do
+{
+    randNum = dice.Next(1, 7);
+    if (randNum >= 4) continue;
+    Console.WriteLine(randNum);
+}
+while (randNum != 3);
+
+
+
+
---
+++ AddLogicToApp/DoWhileAndWhile/RolePlayingGame/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Role Playing Game challenge\n");
+
+
+/* 
+Game Rules
+
+The hero and the monster will start with 10 health points.
+All attacks will be a value between 1 and 10.
+The hero will attack first.
+Print the amount of health the monster lost and their remaining health.
+If the monster's health is greater than 0, it can attack the hero.
+Print the amount of health the hero lost and their remaining health.
+Continue this sequence of attacking until either the monster's health or hero's health is zero or less.
+Print the winner.
+
+ */
+int heroHP = 20;
+int monsterHP = 20;
+
+Random attack = new Random();
+
+do
+{
+    // Hero's attack
+    int heroAtt = attack.Next(1, 11);
+    Console.WriteLine($"Hero rolled {heroAtt}!");
+    monsterHP -= heroAtt;
+    Console.WriteLine($"Monster lost {heroAtt}HP! And now has {monsterHP} HP left.\n");
+    if (monsterHP <= 0) continue;
+
+    int monsterAtt = attack.Next(1, 11);
+    Console.WriteLine($"Monster rolled {monsterAtt}!");
+    heroHP -= monsterAtt;
+    Console.WriteLine($"Hero lost {monsterAtt}HP! And now has  {heroHP} HP left.\n");
+    if (heroHP <= 0) continue;
+
+}
+while (heroHP > 0 && monsterHP > 0);
+
+if (monsterHP <= 0)
+{
+    Console.WriteLine("Hero wins!");
+    monsterHP = 0;
+    Console.WriteLine($"Monster has {monsterHP} HP!");
+}
+//if (heroHP <= 0)
+else
+{
+    Console.WriteLine("Monster wins!");
+    heroHP = 0;
+    Console.WriteLine($"Hero has {heroHP} HP!");
+}
