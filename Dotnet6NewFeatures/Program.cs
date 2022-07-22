9b2b538d2bb7ad9005a0e15f229e6186fe853c27
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-07-22 20:53:13 +0200

    Completed Data type casting and convertion.

---
+++ Dotnet6NewFeatures/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+namespace Dotnet6NewFeatures
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a program that swaps two variable values ");
+
+            int a = 100;
+            int b = 200;
+            Console.WriteLine($"a = {a}, b = {b}");
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+
+        }
+    }
+
+}
