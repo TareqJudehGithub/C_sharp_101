b59c9f88b7bad13523d0a983ba65e24d51cd10b4
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-07-25 14:25:17 +0200

    Completed String Fomarmatting section, exercises, and challenge.

---
+++ StringFormatting/Challenge/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Format alphanumeric data for presentation challenge\n");
+
+
+
+string customerName = "Mr. Jones";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0002m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+Console.WriteLine($@"
+Dear {customerName},
+As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.
+
+Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.
+
+Our new product, Glorious Future offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C2}.
+
+");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+
+string comparisonMessage = "";
+
+
+// Your logic here
+
+
+comparisonMessage += currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+
+Console.WriteLine(comparisonMessage);
+
+Console.WriteLine($"{currentReturn} \t{newReturn:P}");
---
+++ StringFormatting/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("String and numbers Formatting\n");
+
+string first = "Hello";
+string second = "World";
+
+// Composite Formatting
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+Console.WriteLine("\n");
+
+//  String interpolation
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!\n");
+
+// Currency formatting
+decimal foodBill = 25.7517m;
+Console.WriteLine($"Food bill: {foodBill:C2}\n");
+
+// Numbers formatting
+decimal bigNum = 1584195.1254687m;
+Console.WriteLine($"clean, formatted number = {bigNum:N3}\n");
+
+// Percentage formatting
+
+decimal total = 25.458m;
+decimal tax = 0.16m;
+Console.WriteLine($"Tax amount = {total:N2} * {tax:P0} = {total * tax:N2}");
+Console.WriteLine($"Total after tax = {total:N2} +  {total * tax:N2} = {total + (total * tax):N2}\n");
+
+
+// exercise
+int invoiceNumber = 1201;
+decimal productMeasurement = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total2 = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"Product: {productMeasurement:N3} mg");
+
+Console.WriteLine($"Subtotal: {subtotal:C} ");
+Console.WriteLine($"Tax: {taxPercentage:P2}");
+
+Console.WriteLine($"Total: {total2:C} ");
+
