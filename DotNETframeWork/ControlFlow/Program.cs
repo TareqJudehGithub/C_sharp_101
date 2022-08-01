9f7b3e08bf8020243ab3d3745ba6efd1f27aa33f
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-08-01 20:39:41 +0200

    Completed Operators and Control Flow lectures and assignments.

---
+++ DotNETframeWork/ControlFlow/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ControlFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // if-else statements
+            IfElse();
+
+        }
+        static void IfElse()
+        {
+            int age = 47;
+
+            if (age >= 18) Console.WriteLine("You're permitted to obtain a driving license.");
+            else Console.WriteLine("You're still not old enough to obtain a driving license.");
+
+            Console.WriteLine("");
+
+            int score = 34;
+            char grade = ' ';
+
+            if (score >= 85) grade = 'O';
+            else if (score >= 60 && score < 85) grade = 'A';
+            else if (score >= 50 && score < 60) grade = 'B';
+            else if (score >= 35 && score < 50) grade = 'C';
+            else grade = 'F';
+
+            Console.WriteLine($"Your grade is {grade}");
+
+            Console.WriteLine("");
+            HeightCategories();
+        }
+
+        static void HeightCategories()
+        {
+            /* 
+                 Height categories:
+                    Height is less than 150 cm = "Dwarf"
+                    Height is between 150 cm and 165 cm = "Average height"
+                    Height is between 165 cm and 195 cm = "Tall"
+                    Height is above 195 cm = "Abnormal height"
+
+                 Note: 1 inch = 2.54 centimeter
+                 You need to convert the input value (inches) into centimeters.
+
+                  Eg:
+                   Input: 75
+                   Output: Tall
+            */
+
+            decimal heightInInch = 76.8M;
+            decimal heightInCM = heightInInch * 2.54M;
+
+            if (heightInCM < 150) Console.WriteLine("Dwarf");
+            else if (heightInCM >= 150 && heightInCM < 165) Console.WriteLine("Average height");
+            else if (heightInCM >= 165 && heightInCM < 195) Console.WriteLine("Tall");
+            else Console.WriteLine("Abnormal height");
+        }
+    }
+}
+
---
+++ DotNETframeWork/Operators/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Operators\n");
+
+            // arithmetical operators
+            decimal a = 10M;
+            decimal b = 3M;
+
+            decimal c = a + b;
+
+            //Console.WriteLine(c);
+            //Console.WriteLine(a % b);
+
+            // assignment operators
+            a += 20M;
+            a -= 20M;
+            Console.WriteLine(a);
+            a %= 3M;
+            Console.WriteLine(a);
+
+            // comparison operators
+
+            Console.WriteLine(a < b);
+            Console.WriteLine(c < a);
+
+            // logical operators
+            Console.WriteLine(a == b);
+
+            Console.WriteLine((a > b) && (b > c));
+
+            Console.WriteLine($"The area of the circle = {CircleArea()}");
+
+
+            FeetToInches();
+
+        }
+
+        // Methods
+
+        static decimal CircleArea()
+        {
+            /*
+             * This method calculates and returns the area of a circle providing it's radius.
+             */
+
+
+            //double radius = 20;
+            //decimal area = Convert.ToDecimal(Math.PI * Math.Pow(radius, 2));
+
+            decimal radius = 20;
+            decimal area = 3.14159M * (radius * radius);
+            return area;
+        }
+
+        static void FeetToInches()
+        {
+            /*
+             * This method converts users height from feet and inches to centimeters.
+             */
+
+            decimal feet = 6.1M;
+            decimal inches = 0M;
+            decimal heightInInches = (feet * 12) + inches;
+
+            decimal inchToCM = 2.54M;
+
+            decimal heightInCM = heightInInches * inchToCM;
+
+            Console.WriteLine($"{feet} foot and {inches} inches height is {heightInCM} centimeters");
+        }
+
+        static void NearestThouthands()
+        {
+            /* 
+                Instructions
+                If the number's last three digits are greater than or equal to 500; it should "round up" the number.
+                If the number's last three digits are less than 500; it should "round down" the number.
+                If the number is less than 500; it should round up to 1000.
+
+                Eg:
+                Input: 499  Output: 1000
+                Input: 500  Output: 1000
+                Input: 999  Output: 1000
+                Input: 1000 Output: 1000
+                Input: 1499 Output: 1000
+            */
+        }
+    }
+}
