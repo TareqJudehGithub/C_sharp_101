af691998d29e0137bcd43f3a22d114809226cf87
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-08-18 00:01:04 +0200

    Completed Constructors: instance, static, and types of constructors.

---
+++ OOPwebUni/constructor/practice/ClassLibrary1/Class1.cs
@@ -0,0 +1,36 @@
+﻿public class Employee
+{
+    // fields
+    public int empID;
+    public string empName;
+    public string empJob;
+
+    // static fields
+    public static string companyName;
+
+    // constructor
+    public Employee(int id, string name, string job)
+    {
+        this.empID = id;
+        this.empName = name;
+        this.empJob = job;
+    }
+
+    // static constructor
+    static Employee()
+    {
+        companyName = "Test Company";
+    }
+
+
+
+    // Setters
+
+
+    // Getters
+
+    public int GetEmpID()
+    {
+        return empID;
+    }
+}
---
+++ OOPwebUni/constructor/practice/Construct/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Construct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate a n new object from Employee class
+            Employee ali = new Employee(id: 1001, name: "ali", job: "Sales");
+            Employee sarah = new Employee(id: 1002, name: "Sarah", job: "Marketing");
+
+            System.Console.WriteLine(ali.GetEmpID());
+            System.Console.WriteLine(ali.empName);
+            System.Console.WriteLine(Employee.companyName);
+            System.Console.WriteLine(ali.empJob);
+
+            System.Console.WriteLine("");
+
+            System.Console.WriteLine(sarah.GetEmpID());
+            System.Console.WriteLine(Employee.companyName);
+            System.Console.WriteLine(sarah.empName);
+            System.Console.WriteLine(sarah.empJob);
+        }
+    }
+}
