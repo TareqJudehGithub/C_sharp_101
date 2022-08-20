62d7beae6e7fb668bf0664e23c318ab88f9f40eb
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-08-20 20:56:54 +0200

    Completed Properties Box Volume assignment.

---
+++ OOPmasterClass/Properties/Properties/Practice/Volume/Box.cs
@@ -0,0 +1,47 @@
+﻿namespace Volume
+{
+    internal class Box
+    {
+        // fields/properties
+
+        private int Length { get; set; }
+        private int Height { get; set; }
+        private int Width { get; set; }
+        private int Volume
+        {
+            get
+            {
+                return this.Length * this.Height * this.Width;
+            }
+            set
+            {
+                this.Volume = value;
+            }
+        }
+
+        // methods
+
+        internal int CalculateVolume()
+        /* 
+         This method calculates a volume of a box based on user inputs for length, height, and width.
+         */
+        {
+            Console.WriteLine("This program calculates the volume of a box.\nPlease enter box dimensions below:");
+            Console.Write("Length: ");
+            this.Length = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Height: ");
+            this.Height = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Width: ");
+            this.Width = Convert.ToInt32(Console.ReadLine());
+
+            return this.Volume;
+        }
+        internal void DispalayInfo()
+        {
+            Console.WriteLine($"volume = Length {this.Length} * Height {this.Height} * Width {this.Width}\n" +
+                $"Volume = {this.Length * this.Height * this.Width}");
+        }
+    }
+}
---
+++ OOPmasterClass/Properties/Properties/Practice/Volume/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Volume
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate new objects
+            Box box = new Box();
+
+            // Call methods
+            box.CalculateVolume();
+            box.DispalayInfo();
+        }
+    }
+}
