339e1af7e062716d792377b691c8884bce56c89d
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-09-02 18:31:55 +0200

    Completed Introduction to Arrays, foreach, and a challange in Collections module.

---
+++ OOPmasterClass/Collections/Arrays/Foreach/ForEach/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("foreach loop\n");
+
+
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i;
+            }
+
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            // Call methods
+            GreetFriend();
+        }
+
+        internal static void GreetFriend()
+        {
+            string friendName = string.Empty;
+            string[] friendsList = new string[5];
+            int counter = 0;
+
+            while (counter < friendsList.Length)
+            {
+                Console.Write("Enter a new friend name, or  'quit' to finish: ");
+                friendName = Console.ReadLine()!;
+
+                if (friendName.Equals("quit")) break;
+
+                friendsList[counter] = friendName;
+                counter++;
+            }
+
+            foreach (string friend in friendsList)
+            {
+                if (string.IsNullOrEmpty(friend))
+                    Console.WriteLine("Hello, Stranger!");
+                else Console.WriteLine($"Hello, {friend}!");
+            }
+        }
+    }
+}
+
---
+++ OOPmasterClass/Collections/Arrays/Lecture/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arrays\n");
+
+            // Declare a new array
+            int[] grades = new int[5];
+
+            // initialize grades elements with values
+            grades[0] = 92;
+            grades[1] = 82;
+            grades[2] = 87;
+            grades[3] = 89;
+            grades[4] = 77;
+
+            // Access arrays elements
+            Console.WriteLine($"Grade 1 = {grades[0]}");
+
+            // update grades elements values
+            Console.Write("Grade 1: ");
+            grades[0] = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine($"Grade 1 = {grades[0]}\n");
+
+            int[] studentGrades = { 97, 82, 74, 91, 82 };
+
+            // Return array length
+            Console.WriteLine($"studentGrades array length is: {studentGrades.Length}");
+
+
+
+        }
+    }
+}
