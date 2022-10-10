9e7171dca44db81a21bb0205e7b1a35be0c6acd5
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-10-10 13:39:38 +0200

    Completed Strings Manipulation, Exceptions Handling.

---
+++ TrevorCourse/MoreAdvConstructs/DateTimeManipulation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("DateTime class manipulation\n");
+
+
+            // empty DateTime  - initialize a new DateTime object();
+            DateTime date = new DateTime();
+
+            // create a DateTime from date and time
+            DateTime birthday = new DateTime(year: 1975, month: 6, day: 27);
+            Console.WriteLine($"my birthday is in: {birthday}");
+
+            // create a DateTime from current timestamps
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"Today's date: {now}");
+
+            // Today's date in string format - Very useful for users date entry:
+            DateTime stringDate = DateTime.Parse("10/9/2022", CultureInfo.InvariantCulture);
+            Console.WriteLine($"Date in string format: {stringDate}");
+
+            //Console.WriteLine($"The time now is: {now.ToShortDateString()}");
+            // create a DateTime from a string
+
+            // add additional time - like an hour from now
+            now.AddHours(1);
+            Console.WriteLine($"An hour from now, the time is: {now.AddDays(1).AddHours(1)}");
+
+            // DateTime from ticks - New Feature in C# 10
+            Console.WriteLine($"Time as numeral is: {now.Ticks}");
+
+            // date only - New feature in C#
+            DateOnly MyBirthday = DateOnly.FromDateTime(birthday);
+            Console.WriteLine($"Today's date only: {MyBirthday}");
+
+            Console.WriteLine($"Today's date only: {birthday.ToShortDateString()}");
+            Console.WriteLine($"Today's date only: {now.ToShortDateString()}");
+
+            //time only
+            Console.WriteLine($"Time now is: {now.ToShortTimeString()}");
+
+            TimeOnly timeNow = TimeOnly.FromDateTime(now);
+            Console.WriteLine($"Time now is: {timeNow}");
+
+            //SslClientHelloInfo world
+        }
+    }
+}
---
+++ TrevorCourse/MoreAdvConstructs/HandlingExceptions/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace HandlingExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Handling Exceptions\n");
+
+            /* 
+             
+            try - a try block attempts an operation
+            catch - catch any fatal error of exception
+            finally - whether any of the try or catch blocks were executed, execute the finally block of code.
+            throw - interrupts the programs displaying the error occurring.
+             */
+
+            UserInfo();
+        }
+
+        static void UserInfo()
+        {
+            string firstName = string.Empty;
+            string lastName = string.Empty; ;
+            int age = 0;
+            try
+            {
+                Console.Write("First Name: ");
+                firstName = Console.ReadLine();
+
+                Console.Write("Last Name: ");
+                lastName = Console.ReadLine();
+
+                Console.Write("Age: ");
+                age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"First Name: {firstName}\n" +
+                $"Last Name: {lastName}\n" +
+                $"Age: {age}\n");
+            }
+            catch (FormatException err)
+            {
+                Console.WriteLine($"\nError occurred!\nAge must be an Integer number\n{err.Message}\n");
+            }
+            finally
+            {
+                Console.WriteLine("End of program.");
+            }
+
+
+        }
+    }
+}
