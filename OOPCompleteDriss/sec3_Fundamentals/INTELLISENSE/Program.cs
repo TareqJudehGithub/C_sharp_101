77c1335171069c577aa401ae64d96b0f69881278
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-11-07 21:11:15 +0200

    Completed C# Fundamentals module.

---
+++ OOPCompleteDriss/sec3_Fundamentals/INTELLISENSE/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace INTELLISENSE;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Intellisense\n");
+
+        string myString = "I love C#";
+
+        bool isTrue = myString.StartsWith('I');
+        Console.WriteLine(isTrue);
+    }
+}
