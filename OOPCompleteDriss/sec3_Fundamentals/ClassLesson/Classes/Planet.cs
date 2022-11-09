289a4ff027fc2be967f1897e25bbc9ba7744903d
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-11-09 18:01:01 +0200

    Completed Classes lesson, building a small Planet project.

---
+++ OOPCompleteDriss/sec3_Fundamentals/ClassLesson/Classes/Planet.cs
@@ -0,0 +1,50 @@
+﻿namespace Classes
+{
+    internal class Planet
+    {
+        // fields - global variables in the class
+        private int _id;
+        private int _radius;
+        private List<string> _satellites = new();
+
+        // properties
+        internal string Name { get; set; }
+        internal int Radius
+        {
+            set { this._radius = value; }
+            get { return this._radius; }
+        }
+
+        // Constructors -  Constructors are used to initialize fields.
+        internal Planet(int id)
+        {
+            this._id = id;
+        }
+
+        // Methods
+        internal int GetTemperature()
+        {
+            Random random = new Random();
+            int temp = random.Next(0, 40);
+            return temp;
+        }
+        internal void AddSatellite(string name)
+        {
+            _satellites.Add(name);
+        }
+        internal void PrintInfo()
+        {
+            Console.WriteLine(this);
+        }
+        public override string ToString()
+        {
+            string planetInfo = $"Planet ID: {_id}" + Environment.NewLine;
+            planetInfo += $"Name: {Name}" + Environment.NewLine;
+            planetInfo += $"Radius: {Radius} km" + Environment.NewLine;
+            planetInfo += $"{Name} has {_satellites.Count} satellite(s)" + Environment.NewLine;
+
+            return planetInfo;
+        }
+
+    }
+}
---
+++ OOPCompleteDriss/sec3_Fundamentals/ClassLesson/Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Classes");
+
+            // Instantiate new objects from classes
+            var earth = new Planet(1);
+            // OR
+            Planet mars = new Planet(2);
+
+            // Method calls
+            earth.Name = "Earth";
+            earth.Radius = 10000;
+            Console.WriteLine("");
+
+            earth.PrintInfo();
+            Console.WriteLine("");
+
+            int temp = earth.GetTemperature();
+            Console.WriteLine($"Planet {earth.Name} Temp is: {temp}C");
+            Console.WriteLine("");
+
+            earth.AddSatellite("Moon");
+            Console.WriteLine("");
+
+            earth.PrintInfo();
+
+        }
+    }
+}
