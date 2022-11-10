4331df2f5010a4bf222594aca4aa7f4775f542e0
TareqJudehGithub <tareq.joudeh@gmail.com>
2022-11-10 21:17:33 +0200

    Completed Loops, Jump Statements, and Stacks modules.

---
+++ OOPCompleteDriss/sec5_ReferenceValueTypes/ValueTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ValueTypes;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Value Types\n");
+        int i = 0;
+        int j = i;
+        Console.WriteLine($"Before, {nameof(i)}: {i}, {nameof(j)}: {j}");
+        i = 1;
+        Console.WriteLine($"After, {nameof(i)}: {i}, {nameof(j)}: {j}");
+    }
+}
